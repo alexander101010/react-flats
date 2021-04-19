5ecfaf1da59da1a6ae9a189f90e3c45cefb04011
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-19 15:54:32 +0200

    flat and flat_list components, TODO marker

---
+++ src/components/flat_list.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Flat from "./flat.jsx";
+
+const FlatList = (props) => {
+  const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.id}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectFlat={props.selectFlat}
+        />
+      );
+    });
+  };
+
+  return <div className="flat-list">{renderList()}</div>;
+};
+
+export default FlatList;
