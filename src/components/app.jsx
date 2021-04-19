3a3e1fb1cff89f77e1ad8d797c15c5f92789b313
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-19 15:53:53 +0200

    app class, TODO map render

---
+++ src/components/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import FlatList from "./flat_list.jsx";
+import flats from "../../data/flats.js";
+
+console.log(flats);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedFlat: flats[0],
+      flats,
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  };
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={flats}
+          selectFlat={this.selectFlat}
+          selectedFlat={this.state.selectedFlat}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
